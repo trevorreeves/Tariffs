0e3260e914d958d2b476a94c33ae20f00f136b2e
Trevor Reeves <trr@sitecore.net>
2018-03-13 14:43:20 +0000

    added test coverage for simpleFile & Data

---
+++ tests/Tariffs.Data.SimpleFile.Tests/SimpleFileTariffLoaderFixture.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using Xunit;
+
+namespace Tariffs.Data.SimpleFile.Tests
+{
+    public class SimpleFileTariffLoaderFixture
+    {
+        [Fact]
+        public void Load_ValidFileExists_LoadsDataCorrectly()
+        {
+            var actual = SimpleFileTariffLoader.Load(new FileInfo("./data/prices.json"));
+
+            //{ "tariff": "better-energy", "rates": { "power":  0.1367, "gas": 0.0288}, "standing_charge": 8.33},
+            //{ "tariff": "2yr-fixed", "rates": { "power": 0.1397, "gas": 0.0296}, "standing_charge": 8.75},
+            //{ "tariff": "greener-energy", "rates": { "power":  0.1544}, "standing_charge": 8.33},
+            //{ "tariff": "simpler-energy", "rates": { "power":  0.1396, "gas": 0.0328}, "standing_charge": 8.75}
+
+            actual.Should().HaveCount(4);
+
+            actual.Keys.Should()
+                .BeEquivalentTo(new[] {"better-energy", "2yr-fixed", "greener-energy", "simpler-energy"});
+
+            var better = actual["better-energy"];
+            better.Name.Should().Be("better-energy");
+            better.GasRate.Should().Be(0.0288M);
+            better.PowerRate.Should().Be(0.1367M);
+            better.StandingCharge.Should().Be(8.33M);
+
+            var greener = actual["greener-energy"];
+            greener.Name.Should().Be("greener-energy");
+            greener.GasRate.Should().Be(0M);
+            greener.PowerRate.Should().Be(0.1544M);
+            greener.StandingCharge.Should().Be(8.33M);
+        }
+
+        [Fact]
+        public void Load_EmptyFileExists_LoadsEmptyData()
+        {
+            var actual = SimpleFileTariffLoader.Load(new FileInfo("./data/empty-prices.json"));
+
+            actual.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Load_NoFileExists_Throws()
+        {
+            new Action(() => SimpleFileTariffLoader.Load(new FileInfo("./idontexist")))
+                .Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void Load_InvalidJsonFileExists_Throws()
+        {
+            new Action(() => SimpleFileTariffLoader.Load(new FileInfo("./invalid-prices.json")))
+                .Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ tests/Tariffs.Data.Tests/TariffSourceFixture.cs
@@ -0,0 +1,176 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Tariffs.Data.Tests
+{
+    public class TariffSourceFixture
+    {
+        private readonly TariffSource _source;
+
+        public TariffSourceFixture()
+        {
+            _source = new TariffSource();
+        }
+        
+        [Fact]
+        public async Task TryGet_TariffExists_ReturnsTariff()
+        {
+            // arrange
+            var expected = new Tariff("two", 4.0M, 5.0M, 6.0M);
+            await _source.ReloadAsync(() => new Dictionary<string, Tariff>()
+            {
+                { "one", new Tariff("one", 1.0M, 2.0M, 3.0M) },
+                { expected.Name,  expected }
+            });
+
+            // act
+            _source.TryGet("two", out var actual).Should().Be(true);
+
+            // assert
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public async Task TryGet_TariffExistsWithDifferentCasing_ReturnsTariff()
+        {
+            // arrange
+            var expected = new Tariff("two", 4.0M, 5.0M, 6.0M);
+            await _source.ReloadAsync(() => new Dictionary<string, Tariff>()
+            {
+                { "one", new Tariff("one", 1.0M, 2.0M, 3.0M) },
+                { expected.Name,  expected }
+            });
+
+            // act
+            _source.TryGet("TWO", out var actual).Should().Be(true);
+
+            // assert
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public async Task TryGet_TariffDoesntExist_ReturnsFalse()
+        {
+            // arrange
+            await _source.ReloadAsync(() => new Dictionary<string, Tariff>()
+            {
+                { "one", new Tariff("one", 1.0M, 2.0M, 3.0M) },
+            });
+
+            // act
+            _source.TryGet("two", out var actual).Should().Be(false);
+
+            // assert
+            actual.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetAll_TariffsExist_ReturnsAll()
+        {
+            // arrange
+            var one = new Tariff("one", 1.0M, 2.0M, 3.0M);
+            var two = new Tariff("two", 4.0M, 5.0M, 6.0M);
+            await _source.ReloadAsync(() => new Dictionary<string, Tariff>()
+            {
+                { one.Name, one },
+                { two.Name, two }
+            });
+
+            // act
+            var actual = _source.GetAll();
+
+            // assert
+            actual.Should().BeEquivalentTo(new[] {one, two});
+        }
+
+        [Fact]
+        public async Task GetAll_NoTariffs_ReturnsEmpty()
+        {
+            // arrange
+            await _source.ReloadAsync(() => new Dictionary<string, Tariff>());
+
+            // act
+            var actual = _source.GetAll();
+
+            // assert
+            actual.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Find_TariffMatches_ReturnsTariff()
+        {
+            // arrange
+            var expected = new Tariff("two", 4.0M, 5.0M, 6.0M);
+            await _source.ReloadAsync(() => new Dictionary<string, Tariff>()
+            {
+                { "one", new Tariff("one", 1.0M, 2.0M, 3.0M) },
+                { expected.Name,  expected }
+            });
+
+            // act
+            var actual = _source.Find(t => t.PowerRate == 4.0M).ToArray();
+
+            // assert
+            actual.Single().Should().Be(expected);
+        }
+
+        [Fact]
+        public async Task Find_NoTariffMatches_ReturnsNone()
+        {
+            // arrange
+            var expected = new Tariff("two", 4.0M, 5.0M, 6.0M);
+            await _source.ReloadAsync(() => new Dictionary<string, Tariff>()
+            {
+                { "one", new Tariff("one", 1.0M, 2.0M, 3.0M) },
+                { expected.Name,  expected }
+            });
+
+            // act
+            var actual = _source.Find(t => t.PowerRate == 99999M).ToArray();
+
+            // assert
+            actual.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Reload_BruteForceAttack_IsThreadSafe()
+        {
+            // run the test for 10 seconds
+            var cancellation = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            // run a set of concurrent searches continually on the source
+            var searchTasks = Enumerable
+                .Range(1, 10)
+                .Select(i => Task.Run(() =>
+                {
+                    while (!cancellation.Token.IsCancellationRequested)
+                    {
+                        _source.Find(_ => true);
+                    }
+                }));
+
+            // run reloads continually on the source
+            var reloadTask = Task.Run(() =>
+                {
+                    var rand = new Random();
+
+                    while (!cancellation.Token.IsCancellationRequested)
+                    {
+                        _source.ReloadAsync(() =>
+                            Enumerable
+                                .Range(1, rand.Next(5, 100))
+                                .Select(j => new Tariff(j.ToString(), j * 2, j * 3, j * 4))
+                                .ToDictionary(t => t.Name));
+                    }
+                });
+
+            // all tasks should complete without error.
+            await Task.WhenAll(searchTasks.Concat(new[] { reloadTask }));
+        }
+    }
+}
